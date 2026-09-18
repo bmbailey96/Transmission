b98eb50910000293f372e4961b42895642ac0449
bmbailey96 <153319910+bmbailey96@users.noreply.github.com>
2026-09-18 14:09:04 -0600

    Add Transmission V2 invariant tests

---
+++ test/v2Logic.js
@@ -0,0 +1,17 @@
+const assert = require('node:assert/strict');
+const { releaseKey } = require('../lib/v2/store');
+const { lifecycleFor, spotifyBackoffHours } = require('../lib/v2/engine');
+
+assert.equal(releaseKey('Phoebe Bridgers', 'Lost Weekend'), 'phoebe-bridgers__lost-weekend');
+assert.equal(releaseKey('Godspeed You! Black Emperor', 'NO TITLE'), 'godspeed-you-black-emperor__no-title');
+
+assert.equal(lifecycleFor('2026-09-18', '2026-09-18'), 'released');
+assert.equal(lifecycleFor('2026-09-19', '2026-09-18'), 'upcoming');
+assert.equal(lifecycleFor(null, '2026-09-18'), 'upcoming');
+
+assert.equal(spotifyBackoffHours(1), 3);
+assert.equal(spotifyBackoffHours(2), 6);
+assert.equal(spotifyBackoffHours(5), 48);
+assert.equal(spotifyBackoffHours(9), 48);
+
+console.log('Transmission V2 logic checks passed');
